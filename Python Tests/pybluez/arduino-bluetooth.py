2508ba6af8fa6209047dd536a31c0284bd602137
Sven Kohn <sven@mindprobe.io>
2020-04-04 15:03:25 +0000

    edited: Arduino bluetooth test

--- Python Tests/pybluez/arduino-bluetooth.py
+++ Python Tests/pybluez/arduino-bluetooth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import bluetooth
+import time
 
 host = "00:13:EF:00:05:A7"
 port = 1
@@ -10,18 +11,15 @@ sock.connect((host, port))
 
 print("Connected!")
 
-byteList = []
-for i in range(1, 10):
-    byteList.append(255)
-print(byteList)
-
 print("Sending data to Arduino.")
-sock.send(bytes(byteList))
+for i in range(1, 101):
+    sock.send(bytes([i]))
+    time.sleep(0.1)
 
-print("Receiving data from Arduino.")
+""" print("Receiving data from Arduino.")
 data = ""
 while True:
     byteData = sock.recv(100)
     decodedData = byteData.decode('utf-8')
     data += decodedData
-    print(data)
+    print(data) """
